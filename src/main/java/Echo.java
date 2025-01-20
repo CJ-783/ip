f508c8c4eec9a6a5b38bf4e3035500aa11019c6c
Cheng Jing <chengjing32@yahoo.com>
2025-01-20 11:01:58 +0800

    Change welcome message
    Welcome message contains default values (Duke)
    
    Change the default value to "Echo", our selected name for the chatbot
    
    Let's
    * Change the filename to "Echo"
    * Change the welcome banner to contain "Echo"
    * Change filename to "Echo"

---
+++ src/main/java/Echo.java
@@ -0,0 +1,13 @@
+public class Echo {
+    public static void main(String[] args) {
+        String logo = "#######  #####   #     #  ####### \n"
+                    + "#       #     #  #     #  #     # \n"
+                    + "#       #        #     #  #     # \n"
+                    +"#####   #        #######  #     # \n"
+                    +"#       #        #     #  #     # \n"
+                    +"#       #     #  #     #  #     # \n"
+                    +"#######  #####   #     #  #######";
+        System.out.println("Welcome to\n" + logo + "\n" + "What can I do for you?");
+        System.out.println("See you next time");
+    }
+}
