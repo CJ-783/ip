7b1d592e2e2e5ab5fe08a0308346358d00982da2
Cheng Jing <chengjing32@yahoo.com>
2025-01-20 11:20:01 +0800

    Include echo functionality
    Added echo functionality to the chatbot

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Echo {
     public static void main(String[] args) {
         String logo = "#######  #####   #     #  ####### \n"
@@ -8,6 +10,17 @@ public class Echo {
                     +"#       #     #  #     #  #     # \n"
                     +"#######  #####   #     #  #######";
         System.out.println("Welcome to\n" + logo + "\n" + "What can I do for you?");
+
+        Scanner reader = new Scanner(System.in);
+        String userInput = reader.next();
+
+
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = reader.next();
+        }
+
         System.out.println("See you next time");
+
     }
 }
