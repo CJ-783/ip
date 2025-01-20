2c5ed0eb36fd3da18d7fc05b34830fb8a3595985
Cheng Jing <chengjing32@yahoo.com>
2025-01-20 14:26:35 +0800

    Created new class for Task
    When a user type in an input, it will be added as a task object.
    The task entered also contains many different data (e.g. description,
    status etc.).
    
    Let's create a new class called "Task" containing all the different
    data and functionality.

---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
