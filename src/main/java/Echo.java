b999d7e7b9b5514471c17f4bd7a88566cf0bbbc2
Cheng Jing <chengjing32@yahoo.com>
2025-01-20 11:37:23 +0800

    Add list and store functionality
    Current functionality only contains echo.
    
    Let's
    * allow user to store their input
    * return the list that the user stores

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -12,15 +12,28 @@ public class Echo {
         System.out.println("Welcome to\n" + logo + "\n" + "What can I do for you?");
 
         Scanner reader = new Scanner(System.in);
-        String userInput = reader.next();
+        String userInput = reader.nextLine();
 
+        String[] storeText = new String[100];
+        int storeTextAmnt = 0;
 
         while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                for (int i = 0; i < storeTextAmnt; i++) {
+                    System.out.println(i+1 + ". " + storeText[i]);
+                }
+            }
+
+            storeText[storeTextAmnt] = userInput;
+            storeTextAmnt++;
+
             System.out.println(userInput);
-            userInput = reader.next();
+            userInput = reader.nextLine();
         }
 
         System.out.println("See you next time");
 
     }
+
+
 }
