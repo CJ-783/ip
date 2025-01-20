0a02d1c173afd4501779b9048254c4c68ddf32cd
Cheng Jing <chengjing32@yahoo.com>
2025-01-20 16:00:37 +0800

    Update Echo.java
    Current app does not allow the different type of tasks (todo, event,
    deadline).
    
    Let's
    * Add the functionality of events, todo and deadline into our chatbot

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -14,13 +14,13 @@ public class Echo {
         Scanner reader = new Scanner(System.in);
         String userInput = reader.nextLine();
 
-        Task[] storeText = new Task[100];
+        Task[] storeTask = new Task[100];
         int storeTaskAmt = 0;
 
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 for (int i = 0; i < storeTaskAmt; i++) {
-                    System.out.println(i+1 + "." +  storeText[i].toString() );
+                    System.out.println(i+1 + "." +  storeTask[i].toString() );
                 }
                 userInput = reader.nextLine();
                 continue;
@@ -28,24 +28,27 @@ public class Echo {
 
             if (userInput.contains("unmark")) {
 
-                updateMarkList(false, userInput, storeText);
+                updateMarkList(false, userInput, storeTask);
 
                 userInput = reader.nextLine();
                 continue;
 
             } else if (userInput.contains("mark")) {
 
-                updateMarkList(true, userInput, storeText);
+                updateMarkList(true, userInput, storeTask);
 
                 userInput = reader.nextLine();
                 continue;
             }
 
-                storeText[storeTaskAmt] = new Task(userInput);
-                storeTaskAmt++;
+            String type = userInput.split(" ")[0];
 
-                System.out.println("added: " + userInput);
-                userInput = reader.nextLine();
+            updateList(type, storeTask, storeTaskAmt, userInput);
+
+            System.out.println("Got it! I've added this task: \n" + storeTask[storeTaskAmt].toString() );
+
+            storeTaskAmt++;
+            userInput = reader.nextLine();
 
         }
 
@@ -53,21 +56,46 @@ public class Echo {
 
     }
 
-    private static void updateMarkList(boolean markStatus, String inputIndex, Task[]  storeText) {
+    private static void updateMarkList(boolean markStatus, String inputIndex, Task[]  storeTask) {
         String element = inputIndex.split(" ")[1];
         int elementIndex = Integer.parseInt(element) - 1;
 
         if (markStatus) {
-            storeText[elementIndex].setDone(true);
+            storeTask[elementIndex].setDone(true);
 
             System.out.println("Nice! Marked!");
 
         } else {
-            storeText[elementIndex].setDone(false);
+            storeTask[elementIndex].setDone(false);
             System.out.println("OK, I've marked this task as not done yet:");
         }
 
-        System.out.println(storeText[elementIndex].toString());
+        System.out.println(storeTask[elementIndex].toString());
+
+    }
+
+    private static void updateList(String type, Task[] storeTask, int storeTaskAmt, String userInput) {
+
+        if (type.equals("todo")) {
+            userInput = userInput.substring(5);
+            storeTask[storeTaskAmt] = new Todo(userInput);
+        }
+        if (type.equals("deadline")) {
+            userInput = userInput.substring(9);
+            String description = userInput.split("/")[0];
+
+            String to = userInput.split("/")[1].replace("by ", "");
+            storeTask[storeTaskAmt] = new Deadline(description, to);
+        }
+        if (type.equals("event")) {
+            userInput = userInput.substring(6);
+            String description = userInput.split("/")[0];
+
+            String from = userInput.split("/")[1].replace("from ", "");
+            String to = userInput.split("/")[2].replace("to ", "");
+            storeTask[storeTaskAmt] = new Event(description, from, to);
+        }
+
 
     }
 
