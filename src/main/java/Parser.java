4e02389b914315f4a32c4c862d31b46589857a76
Cheng Jing <chengjing32@yahoo.com>
2025-01-27 17:46:08 +0800

    Change the Basic functionality of the chatbot into OOP
    The current code may result in difficulty maintaining.
    
    This is due to the code not fully utilizing OOP standard.
    
    Let's change the basic functionality into OOP standard before adding
    the rest.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,119 @@
-package PACKAGE_NAME;public class Parser {
+/*
+This class takes care of UNDERSTANDING user's input
+ */
+
+public class Parser {
+    private TaskList taskList;
+    private Ui ui;
+
+    Parser(Ui ui, TaskList taskList) {
+        this.ui = ui;
+        this.taskList = taskList;
+    }
+
+
+    public int getOption(String userInput) {
+        if (userInput.equals("bye")) {
+            ui.exitMessage();
+            return 0;
+        } else if (userInput.equals("list")) {
+            ui.printList(taskList);
+            return 1;
+        } else if (userInput.contains("unmark")) {
+            unMark(userInput);
+            return 1;
+        } else if (userInput.contains("mark")) {
+            mark(userInput);
+            return 1;
+        } else {
+            int option = addRemoveList(userInput);
+            return option;
+        }
+    }
+
+    private int unMark(String userInput) {
+        String index = userInput.split(" ")[1];
+        int unmarkIndex = Integer.parseInt(index) - 1;
+        taskList.setUnmark(unmarkIndex);
+        ui.unmark(unmarkIndex, taskList);
+        return unmarkIndex;
+    }
+
+    private void mark(String userInput) {
+        String index = userInput.split(" ")[1];
+        int markIndex = Integer.parseInt(index) - 1;
+        taskList.setMark(markIndex);
+        ui.mark(markIndex, taskList);
+    }
+
+    private int addRemoveList(String userInput) {
+        String option = userInput.split(" ")[0];
+
+        try {
+            validOption(option);
+            updateList(userInput);
+            return 1;
+
+        } catch (EchoIncorrectOption err) {
+            ui.errorMessage("This option doesn't exist! Please try again");
+            return -1;
+        } catch (ArrayIndexOutOfBoundsException err) {
+            ui.errorMessage("Do remember to add the description");
+            return -2;
+        }
+    }
+
+    private void updateList(String userInput) {
+        String option = userInput.split(" ")[0];
+
+        if (option.equals("delete")) {
+            int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+            ui.deleteFromList(index, taskList);
+            taskList.removeTask(index);
+
+        } else {
+            userInput = userInput.split(" ", 2)[1];
+            String description = userInput.split("/")[0];
+
+            switch (option) {
+                case "todo" -> {
+                    //TODO: REMOVE
+                    System.out.println("todo");
+                    taskList.addTask(new Todo(userInput));
+
+                }
+                case "deadline" -> {
+                    //TODO: REMOVE
+                    System.out.println("deadline");
+                    String to = userInput.split("/by ")[1];
+
+                    taskList.addTask(new Deadline(description, to));
+                    taskList.setDateTime(taskList.getTotalTask() - 1, to);
+
+                }
+                case "event" -> {
+                    //TODO: REMOVE
+                    System.out.println("event");
+                    String from = userInput.split("/")[1].replace("from ", "");
+                    String to = userInput.split("/to ")[1];
+                    taskList.addTask(new Event(description, from, to));
+                    taskList.setDateTime(taskList.getTotalTask() - 1, to);
+
+                }
+            }
+            ui.addToList(taskList);
+        }
+    }
+
+    private void validOption(String userInput) throws EchoIncorrectOption {
+        String[] correctOptions = {"delete", "todo", "deadline", "event"};
+
+        for (String correctOption : correctOptions) {
+            if (userInput.equals(correctOption)) {
+                return;
+            }
+        }
+        throw new EchoIncorrectOption();
+    }
+
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class TaskList {
+import java.util.ArrayList;
+
+public class TaskList {
+
+    public int totalTask = 0;
+
+    ArrayList<Task> storeTask = new ArrayList<Task>();
+
+    TaskList() {
+
+    }
+
+    public int getTotalTask() {
+        return totalTask;
+    }
+
+    public void setDateTime(int index, String dateLine) {
+        storeTask.get(index).setDateTime(dateLine);
+    }
+
+    public void addTask(Task task) {
+        storeTask.add(task);
+        totalTask++;
+    }
+
+    public void removeTask(int index) {
+        storeTask.remove(index);
+        totalTask--;
+    }
+
+    public void setUnmark(int index) {
+        storeTask.get(index).setDone(false);
+    }
+
+    public void setMark(int index) {
+        storeTask.get(index).setDone(true);
+    }
+
+    public void printElementString(int index) {
+        System.out.println(storeTask.get(index).toString());
+
+    }
+
+    public String toString() {
+        StringBuilder outputList = new StringBuilder();
+        String newline = "";
+        for (int i = 0; i < totalTask; i++) {
+            outputList.append(newline);
+            outputList.append(i + 1 + "." + storeTask.get(i).toString());
+            newline = "\n";
+        }
+        return outputList.toString();
+    }
+
+
 }
