085016e7d3d55d3506f70740079751bc7baf09c6
Cheng Jing <chengjing32@yahoo.com>
2025-01-28 21:43:31 +0800

    Bug: Adding an event contains an additional output on screen

--- src/test/java/echo/tasks/EventTest.java
+++ src/test/java/echo/tasks/EventTest.java
@@ -8,7 +8,7 @@ public class EventTest {
 
     @Test
     public void testBasicInput() {
-        String expectedAnswer = "[E][ ] Weekly meeting(from: 28/1/2025 1500to: 28/1/2025 1700)";
+        String expectedAnswer = "[E][ ] Weekly meeting (from: 28/1/2025 1500 to: 28/1/2025 1700)";
         Event e = new Event("Weekly meeting", "28/1/2025 1500", "28/1/2025 1700");
         assertEquals(expectedAnswer, e.toString());
     }
