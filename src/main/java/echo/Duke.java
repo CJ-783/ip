30f983cb3a0e2bfe0a3fb6f4da66afcb8727cb7d
Cheng Jing <chengjing32@yahoo.com>
2025-02-08 09:36:28 +0800

    Remove unnecessary comments

--- src/main/java/echo/Duke.java
+++ src/main/java/echo/Duke.java
@@ -2,18 +2,12 @@ package echo;
 
 public class Duke {
 
-//    private Echo echo;
-
     Duke() {
-//        this.echo = echo;
     }
     public static void main(String[] args) {
-//        System.out.println("Hello!");
     }
 
-//    public String getResponse(String input) {
-//        return "Duke heard: " + input;
-//    }
+
 
 
     public String getResponse(String input) {
