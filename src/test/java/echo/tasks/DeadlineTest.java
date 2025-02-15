f7dbd436622b2d32d96cd013f1ae1dc8dd24bb70
Cheng Jing <chengjing32@yahoo.com>
2025-02-15 15:22:07 +0800

    Bug fix: test case for deadline cause an error if not handled

--- src/test/java/echo/tasks/DeadlineTest.java
+++ src/test/java/echo/tasks/DeadlineTest.java
@@ -4,13 +4,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Date;
+
+import echo.exceptions.DateFormatError;
+
 public class DeadlineTest {
 
     @Test
     public void testBasicInput() {
         String expectedAnswer = "[D][ ] Weekly meeting (by: Jan 28 2025)";
-        Deadline d = new Deadline("Weekly meeting", "28/1/2025 1500");
-        assertEquals(expectedAnswer, d.toString());
+        try {
+            Deadline d = new Deadline("Weekly meeting", "28/1/2025 1500");
+            assertEquals(expectedAnswer, d.toString());
+        } catch (DateFormatError err) {
+            
+        }
+
     }
 
 
